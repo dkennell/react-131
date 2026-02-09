58f79e8edcf72317306a33be481702ba3f6910f0
David Kennell <davidkennell0@gmail.com>
2026-02-09 15:29:37 -0600

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 export default function Home() {
+	function myFunc<T>(value: T) {
+		const finalValue = value * 3
+		return finalValue
+	}
   return (
-	<></>	
+	<>
+		<p>The value is: {myFunc(5)}</p>
+	</>	
   );
 }
